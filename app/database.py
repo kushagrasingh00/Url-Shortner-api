5e38c40cd573c9d4e971159381b4dbd48662024f
Kushagra Singh <kushagrasingh1105@gmail.com>
2026-09-19 23:21:55 +0530

    deploy test line

--- app/database.py
+++ app/database.py
@@ -8,6 +8,9 @@ db_url="DATABASE_URL"
 engine = create_engine(db_url)
 session=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
+print(f"DB URL VALUE: {db_url}")
+print(f"DB URL TYPE: {type(db_url)}")
+
 # dependency
 def get_db():
     db=session()
