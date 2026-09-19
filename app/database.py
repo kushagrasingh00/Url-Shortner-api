a2982d7f85be997a5e7081e1cc3e4cd4fb4602f4
Kushagra Singh <kushagrasingh1105@gmail.com>
2026-09-19 23:26:07 +0530

    final fix

--- app/database.py
+++ app/database.py
@@ -1,15 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
 load_dotenv()
 
-db_url="DATABASE_URL"
+db_url=os.getenv("DATABASE_URL")
 engine = create_engine(db_url)
 session=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
-print(f"DB URL VALUE: {db_url}")
-print(f"DB URL TYPE: {type(db_url)}")
 
 # dependency
 def get_db():
